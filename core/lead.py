16b28ebfe45ba1e35c349f546daaf82f3dc0f643
shahja16 <shahja16@gmail.com>
2022-03-20 23:08:17 +0530

    completed lead feature developement.
    Handling over the lead feature to QA team for testing purpose.

--- core/lead.py
+++ core/lead.py
@@ -1,1 +1,18 @@
-lead.py
+##
+#lead feature functionalities
+##
+
+
+#create lead.
+
+def create_lead():
+	print "created lead successfully"
+
+# update Lead.
+dev update_lead():
+ 	print "Lead updated successfully
+
+
+ #Delete lead
+ def delete_lead():
+ 	print "Lead deleted sucessfully"
